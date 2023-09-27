9ead00f0270399ea736b1163ad1b001e085f109b
ayeshasikander <sikanderayesha46@gmail.com>
2023-09-27 17:57:38 +0500

    complete code

--- javascript.js
+++ javascript.js
@@ -12,7 +12,32 @@ const throttleFunction = (func, delay) => {
     }
 }
 document.querySelector("#center")
-    .addEventListener("mousemove", throttleFunction(() => {
+    .addEventListener("mousemove", throttleFunction((detail) => {
         var div = document.createElement("div");
-        div.classList("imgdiv");
-    }, 500));
+        div.classList.add("imgdiv");
+        div.style.left = detail.clientX + "px";
+        div.style.top = detail.clientY + "px";
+        div.style.transform = "rotate(-5deg)";
+
+        var img = document.createElement("img");
+        img.setAttribute("src", "https://i.pinimg.com/222x/a1/71/0c/a1710ced29d52624ae10103c00b588ae.jpg")
+        div.appendChild(img)
+
+        document.body.appendChild(div);
+
+        gsap.to(img, {
+            y: "0",
+            ease: Power2,
+            duration: .5,
+        });
+
+        gsap.to(img, {
+            y: "100%",
+            delay: .6,
+            ease: Power2,
+        });
+
+        setTimeout(function () {
+            div.remove();
+        }, 2000)
+    }, 300));
